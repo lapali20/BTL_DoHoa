7653ab2474c04f0fc62aa37e074d35b2724c81cb
xuongthin <xuongthin2000@gmail.com>
2020-11-18 21:58:02 +0700

    Init MiniTest6

--- Code/script.js
+++ Code/script.js
@@ -1,13 +1,11 @@
 
-import DirectionalLight from "./light.js";
-import Test from "./light.js";
+
 
 var clock = new THREE.Clock();
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var interactableObjList = [];
-
+camera.position.z = 3;
 var renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -28,106 +26,122 @@ window.addEventListener( 'resize', function () {
     camera.updateProjectionMatrix();
 })
 
-var controls = new THREE.FirstPersonControls(camera, renderer.domElement);
-    controls.lookSpeed = 0.2;
-    controls.movementSpeed = 10;
-    controls.noFly = true;
-    controls.lookVertical = true;
-    controls.constrainVertical = true;
-    controls.verticalMin = 1.0;
-    controls.verticalMax = 2.0;
-    controls.lon = -150;
-    controls.lat = 120;
+// var mesh;
+// let mtlLoader = new THREE.MTLLoader();
+// let objLoader = new THREE.OBJLoader();
+// mtlLoader.load('models/Room_Model.mtl', (materials) => {
+//   materials.preload();
+//   objLoader.setMaterials(materials);
+//   objLoader.load('models/Room_Model.obj', (object) => {
+//     mesh = object;
+//     scene.add(object);
+//   })
+// });
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1);
+// ------ danh sách models
+var models = {
+    // room: {
+    //     obj:"models/room.obj",
+    //     mtl:"models/room.mtl",
+    //     mesh: null,
+    // },
+    Room_Model: {
+        obj:"models/Room_Model.obj",
+        mtl:"models/Room_Model.mtl",
+        posX: -5,
+        posY: 0,
+        posZ: 4
+    }
+}
 
-var cubeMaterials = 
-[
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('models/pink.png'), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('models/pink.png'), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('models/blue.png'), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('models/blue.png'), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('models/gray.png'), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('models/gray.png'), side: THREE.DoubleSide})
-]
+var meshes = {}
 
-var material = new THREE.MeshFaceMaterial( cubeMaterials );
-var cube = new THREE.Mesh( geometry, material);
-scene.add(cube);
+var loadObjects = function() {
+    for( var _key in models ){
+		(function(key){
+			
+            var mtlLoader = new THREE.MTLLoader();
+			mtlLoader.load(models[key].mtl, (materials) => {
+				materials.preload();
+				
+				var objLoader = new THREE.OBJLoader();
+				
+                objLoader.setMaterials(materials);
+				objLoader.load(models[key].obj, (object) => {
+                    scene.position.set(models[key].posX, models[key].posY, models[key].posZ);
+					scene.add(object);
+				});
+			});
+			
+		})(_key);
+    }
+}
 
-var cube1 = new THREE.Mesh( geometry, material);
-scene.add(cube1);
-cube1.position.x = 2
-
-// var room = new THREE.GLTFLoader();
-
-// var dracoLoader = new THREE.DRACOLoader();
-// // dracoLoader.setDecoderPath( '/js/libs/draco/' );
-// room.setDRACOLoader( dracoLoader );
-
-// room.load(
-// 	'models/room.gltf',
-// 	function ( gltf ) {
-
-// 		scene.add( gltf.scene );
-
-// 		gltf.animations; 
-// 		gltf.scene;
-// 		gltf.scenes;
-// 		gltf.cameras;
-// 		gltf.asset;
-
-// 	},
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-var mesh;
-let mtlLoader = new THREE.MTLLoader();
-let objLoader = new THREE.OBJLoader();
-mtlLoader.load('models/Room_Model.mtl', (materials) => {
-  materials.preload();
-  objLoader.setMaterials(materials);
-  objLoader.load('models/Room_Model.obj', (object) => {
-    mesh = object;
-    scene.add(object);
-  })
-});
-
-camera.position.z = 3;
-
+// ------ anh sang
 const light = new THREE.DirectionalLight( 0xffffff, 1, 100 );
 light.position.set( 0, 0, 0 ); //default; light shining from top
 light.castShadow = true; // default false
 scene.add( light );
 
-var directionalLight = new DirectionalLight(scene, 0, 0, 0);
-
 var update = function() {
-    camera.position.y = 2;
     light.position.y += 0.01;
 };
 
+// ------ phần điều khiển camera
+var controls = new THREE.PointerLockControls(camera, renderer.domElement);
+
+controls.getObject().position.y += 2;
+
+let startBtn = document.querySelector("#start-btn");    // nhan de bat dau khoa con tro
+startBtn.addEventListener('click', ()=> {
+    controls.lock();
+    startBtn.style.display = "none";
+})
+
+controls.addEventListener('unlock', ()=> {
+    startBtn.style.display = "inline-block";
+})
+
+let keyboard = []
+addEventListener('keydown', (e) => {
+    keyboard[e.key] = true;
+})
+
+addEventListener('keyup', (e) => {
+    keyboard[e.key] = false;
+})
+
+function processKeyboard() {
+    let speed = 6;
+    let actualSpeed = speed * clock.getDelta();
+    if (keyboard['w']) {
+        controls.moveForward(actualSpeed);
+    }
+    if (keyboard['s']) {
+        controls.moveForward(-actualSpeed);
+    }
+
+    if (keyboard['d']) {
+        controls.moveRight(actualSpeed);
+    }
+    if (keyboard['a']) {
+        controls.moveRight(-actualSpeed);
+    }
+}
+
+
+// ---------
 var render = function() {
-    
-    var delta = clock.getDelta();
     renderer.render( scene, camera );
-    controls.update(delta);
 };
 
 var GameLoop = function() {
     requestAnimationFrame( GameLoop );
     // console.log(camera.position);
+    processKeyboard()
     update();
     render();
 };
 
+loadObjects();
 GameLoop();
